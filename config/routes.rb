317daa8c3fadbe0117e17f3983c2103623bc3409
wing21rain <icesword2021@gmail.com>
2025-06-28 10:42:36 +0900

    fix: add GET route for chests/open

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'chests', to: 'chests#index'
+  get 'chests/open', to: 'chests#open'    # ←追加
   post 'chests/open', to: 'chests#open'
   post 'chests/reset', to: 'chests#reset'
   root "chests#index"
